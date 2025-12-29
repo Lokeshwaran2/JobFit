3083ecb6356a115a2b8a05c4dbb9b86c6648d864
Lokeshwaran R <71618096+Lokeshwaran2@users.noreply.github.com>
2025-12-29 22:50:39 +0530

    bug fix on next.config.js

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  serverExternalPackages: ["@napi-rs/canvas", "pdf-parse"],
+  serverExternalPackages: ["@napi-rs/canvas", "pdf-parse", "mammoth"],
+  transpilePackages: ["@react-pdf/renderer"],
+  webpack: (config) => {
+    config.resolve.alias.canvas = false;
+    return config;
+  },
 };
 
 export default nextConfig;
