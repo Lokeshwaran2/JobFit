0fd0adef8b563cd38c96b6e122a51377b2383718
Lokeshwaran R <71618096+Lokeshwaran2@users.noreply.github.com>
2026-01-20 20:32:24 +0530

    update on robot.ts file

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,6 @@ export default NextAuth(authConfig).auth;
 
 export const config = {
     matcher: [
-        '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|resume-for).*)',
+        "/((?!api|_next/static|_next/image|favicon.ico|robots.txt|sitemap.xml).*)",
     ],
 };
